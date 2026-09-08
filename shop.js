4eaa2b0715ffba57a879ff4d8abf0a2bedeb64b3
lgxpowerna-gif <lgxpowerna@gmail.com>
2026-09-08 13:09:59 -0400

    Add Stripe payment links used by the boutique

---
+++ shop.js
@@ -0,0 +1,6 @@
+const PRODUCTS = {
+  brut: { titleKey:"brut_t", subKey:"brut_sub", hintKey:"brut_hint", requireText:false, stripe:{ ca:"https://buy.stripe.com/14AaEWaiaawY04geiLcfK09", us:"https://buy.stripe.com/9B6eVc8a2eNe3gs6QjcfK0a" } },
+  imprimee: { titleKey:"imp_t", subKey:"imp_sub", hintKey:"order_hint", requireText:true, stripe:{ ca:"https://buy.stripe.com/9B67sK0HA6gI3gsa2vcfK0b", us:"https://buy.stripe.com/aFabJ00HA5cEg3ecaDcfK0c" } },
+  complete: { titleKey:"att_t", subKey:"att_sub", hintKey:"order_hint", requireText:true, stripe:{ ca:"https://buy.stripe.com/7sYbJ0fCugVmg3ea2vcfK0d", us:"https://buy.stripe.com/4gMeVccqidJa18kgqTcfK0e" } },
+  aimants: { titleKey:"aim_t", subKey:"aim_sub", hintKey:"order_hint", requireText:true, stripe:{ ca:"https://buy.stripe.com/6oUbJ03TM7kM3gsdeHcfK0f", us:"https://buy.stripe.com/fZu5kC8a2bB218k4IbcfK0g" } }
+};
